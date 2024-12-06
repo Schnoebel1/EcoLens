8c2ffeb02dbfb8b50b5796a7d5325dce8108bbfe
Schnöbel/Nicolas <naldebert@t-online.de>
2024-12-06 21:07:29 +0100

    port update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,20 @@
-// vite.config.mjs
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Exportiere die Konfiguration mit dem defineConfig-Hilfswerkzeug
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000 , // Optional, wenn du einen spezifischen Port brauchst
+  },
+  base: process.env.NODE_ENV === 'production' ? '/my-app/' : '/',
+  build: {
+    minify: 'esbuild', // Schnellere Minifizierung mit esbuild
+    cssCodeSplit: true, // Teilt CSS für eine bessere Ladeperformance
+    sourcemap: false, // Deaktiviert Sourcemaps im Produktionsbuild
+    chunkSizeWarningLimit: 500, // Gibt eine Warnung aus, wenn die Chunks zu groß sind
+  },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV), // Setzt Umgebungsvariablen
+  },
 });
