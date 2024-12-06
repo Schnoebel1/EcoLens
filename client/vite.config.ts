a04ae1ccf1cd0cc3a91d6ba9c1d6ac678c809504
Nicolas Aldebert <naldebert@t-online.de>
2024-12-07 00:03:59 +0100

    config update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react';
 // Exportiere die Konfiguration mit dem defineConfig-Hilfswerkzeug
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000 , // Optional, wenn du einen spezifischen Port brauchst
-  },
-  base: process.env.NODE_ENV === 'production' ? '/my-app/' : '/',
-  build: {
-    minify: 'esbuild', // Schnellere Minifizierung mit esbuild
-    cssCodeSplit: true, // Teilt CSS für eine bessere Ladeperformance
-    sourcemap: false, // Deaktiviert Sourcemaps im Produktionsbuild
-    chunkSizeWarningLimit: 500, // Gibt eine Warnung aus, wenn die Chunks zu groß sind
-  },
-  define: {
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV), // Setzt Umgebungsvariablen
-  },
 });
